b4d2e5eed722a775331adb40d2df1b135337bba2
CaioRuizz <caio.ruiz00@gmail.com>
2023-05-10 20:58:50 -0300

    subindo codigo

--- tests/__main__.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-if __name__ == '__main__':
-    test_suite = unittest.defaultTestLoader.discover('./tests', pattern='test*.py')
-    test_runner = unittest.TextTestRunner()
-    test_runner.run(test_suite)
